6064df2f5ccc2b40ba3621f0fec9a05f1edc4675
Michael Zürcher <michzuerch@gmail.com>
2021-11-05 19:05:50 +0100

    All pages are there, tailwind works

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,3 +4,4 @@
 
 <!-- Try some classes here -->
 <h1 class="uppercase text-indigo-500">Hello People of Earth</h1>
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+Herzlich Willkommen! Gebrauchtwagen ab 500,- € ---Finanzierung ab 1000,- € Wir verkaufen auch Ihr Auto!!!
+Privat an Privat = günstig Öffnungszeiten: Montag – Freitag : 10.00 - 18.00 Uhr Samstag : 10.00 - 14.00
+Uhr Manfred Schlett Herrenlandstr. 39 78315 Radolfzell Tel. +49 - (0)7732-942407 Fax. +49 - (0)7732-942408
+email. info@billig-car.de Industriegebiet West, gegenüber KAWASAKI +AUTO BOSCH DIENST
