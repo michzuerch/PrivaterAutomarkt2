794a38adf3674632fcb578600563dc1f6e6a3778
Michael Zürcher <michzuerch@gmail.com>
2021-11-07 17:35:27 +0100

    Page start with uppercase letter, NavigationHeader in template

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,6 @@
 <script>
 	import '../styles/tailwind-output.css';
+	import NavigationHeader from '../components/NavigationHeader.svelte';
 </script>
 
-<!-- Try some classes here -->
-<div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-	<h1 class="uppercase text-gray-800">Privater Automarkt Radolfzell</h1>
-	<slot />
-</div>
+<NavigationHeader />
