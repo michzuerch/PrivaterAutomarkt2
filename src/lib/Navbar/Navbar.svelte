9a2f7044abb67cb9300f96fa5828368e6801bfd9
Michael Zürcher <michzuerch@gmail.com>
2021-12-07 09:32:14 +0100

    Working navbar

--- src/lib/Navbar/Navbar.svelte
+++ src/lib/Navbar/Navbar.svelte
@@ -1,36 +1,47 @@
-Navbar
-
 <!-- Navbar goes here
  -->
 <nav class="bg-gray-100">
-	<div class="max-w-6xl mx-auto border border-red-400">
+	<div class="max-w-6xl mx-auto">
 		<div class="flex justify-between">
-			<div class="flex">
+			<div class="flex space-x-4">
 				<!-- Logo -->
 				<div class="">
-					<a href="/Playground">
+					<a href="/" class="flex items-center py-5 px-2 text-gray-700">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
-							class="h-6 w-6"
-							fill="none"
+							class="h-6 w-6 mr-1 text-blue-400"
+							width="24"
+							height="24"
 							viewBox="0 0 24 24"
+							stroke-width="2"
 							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
 						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-							/>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<circle cx="7" cy="17" r="2" />
+							<circle cx="17" cy="17" r="2" />
+							<path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5" />
 						</svg>
-						<span>Privater Automarkt</span>
+						<span class="font-bold">Privater Automarkt</span>
 					</a>
 				</div>
 				<!-- Primary nav -->
-				<div class="">Primary</div>
+				<div class="hidden md:block flex items-center space-x-1">
+					<a href="/About" class="py-5 px-3 text-gray-700 hover:text-gray-900">About</a>
+					<a href="/Hero" class="py-5 px-3 text-gray-700 hover:text-gray-900">Hero</a>
+				</div>
 			</div>
 			<!-- Secondary nav -->
-			<div class="">Secondary</div>
+			<div class="hidden md:block flex items-center space-x-1">
+				<a href="/Login" class="py-5 px-3 ">Login</a>
+				<a
+					href="/Signup"
+					class="py-2 px-3 bg-yellow-400 text-yellow-900 rounded hover:bg-yellow-300 hover:text-yellow-800 transition duration-300"
+					>Signup</a
+				>
+			</div>
 		</div>
 	</div>
 	<!-- Mobile menu -->
