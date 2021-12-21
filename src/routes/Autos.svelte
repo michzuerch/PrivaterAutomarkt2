d209bd154f0a4e2bfdebb959aacba0f0df08a523
Michael Zürcher <michzuerch@gmail.com>
2021-12-21 13:31:36 +0100

    Cleanout

--- src/routes/Autos.svelte
+++
@@ -1,5 +0,0 @@
-<article>
-	<section>
-		<h1>Unsere Autos</h1>
-	</section>
-</article>
--- src/routes/Finanzierung.svelte
+++
@@ -1,9 +0,0 @@
-<article>
-	<section>
-		<h1>Finanzierung</h1>
-		Ihnen gefällt unser Preis-/ Leistungsangebot, aber der Kaufpreis überschreitet Ihr Budget ? Daran soll Ihr Wunsch
-		nach einem Fahrzeug nicht scheitern! Wir bieten Ihnen neben preiswerten Fahrzeugen auch zinsgünstige Kredite an.
-		Es gibt auch keine langen Wartezeiten für die Kreditentscheidung. Nachdem die Kreditanfrage online abgeschickt wurde,
-		liegt innerhalb weniger Minuten eine Entscheidung vor.
-	</section>
-</article>
